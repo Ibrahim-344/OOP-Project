de2aaa21c1ecc80e0f1899bb8ff4015393a6c361
Ibrahim-344 <ihsanibrahim344@gmail.com>
2025-04-06 11:34:56 +0500

    Updated code

--- main.cpp
+++ main.cpp
@@ -1,37 +1,23 @@
 #include<iostream>
 using namespace std;
 
+
 class Student {
-    public:
-        string name;
-        int age;
-        int roll_no;
-        void display() {
-            cout << "Name: " << name << endl;
-            cout << "Age: " << age << endl;
-            cout << "Roll No: " << roll_no << endl;
-        }
+public:
+    int id;
+    string name;
+
+    void printDetails() const { // notice const here
+        cout << id << " " << name << endl;
+    }
 };
-void display(Student s) {
-    cout << "Name: " << s.name << endl;
-    cout << "Age: " << s.age << endl;
-    cout << "Roll No: " << s.roll_no << endl;
-}
-void display(Student *s) {
-    cout << "Name: " << s->name << endl;
-    cout << "Age: " << s->age << endl;
-    cout << "Roll No: " << s->roll_no << endl;
-}
 int main() {
-    Student s1;
-    s1.name = "Ibrahim";
-    s1.age = 20;
-    s1.roll_no = 101;
-    s1.display();
-    display(s1);
-    display(&s1);
+    Student student1;
+    student1.id = 1;
+    student1.name = "Ibrahim";
+
+    // Call the printDetails method
+    student1.printDetails();
+
     return 0;
 }
-
- // Output
- // Name: Ibrahim
